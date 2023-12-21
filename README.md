f16d13a98d319a998cc2ed77f6f81e4d07bc268d
roshankc123 <kcroshan566@gmail.com>
2023-12-21 23:57:20 +0545

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# MarginTop Solutions Website
-
-This is the website of MarginTop Solutions. Visit to know about us !!!
--- src/wrapper/index.js
+++
@@ -1,9 +0,0 @@
-export default function SectionWrapper(Component, id) {
-    return function comp(props) {
-        return (
-            <section className='px-12 xl:px-24' id={id}>
-                <Component {...props} />
-            </section>
-        );
-    }
-}
