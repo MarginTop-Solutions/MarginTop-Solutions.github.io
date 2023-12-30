42faee2f1e947900ce51fcc5697cf70ff0748088
SunX-1224 <sunilsapkota1224@gmail.com>
2023-12-30 23:56:35 +0545

    Updates

---
+++ _next/static/E5IbbQM7qT6gt3kwNNJAQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/275-05a94d452229456e.js","static/chunks/pages/index-a0410074af3b31e5.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
