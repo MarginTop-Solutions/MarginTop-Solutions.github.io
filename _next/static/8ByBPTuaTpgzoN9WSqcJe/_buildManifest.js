7161f8aaa14508b85d898508dab8cbe47f939fc7
Bibek Chalise <modernbib@gmail.com>
2024-02-10 22:41:58 +0545

    Updates

--- _next/static/8ByBPTuaTpgzoN9WSqcJe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/505-1be16f9c67c67171.js","static/chunks/pages/index-7d825f567b4b87b2.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
